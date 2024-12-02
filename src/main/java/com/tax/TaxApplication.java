2a2e3cb3c86530a6265924ff96dd6465e894b6de
Sandra Mendes <sandra.mendes@gmail.com>
2024-12-02 12:01:24 -0300

    Add TaxApplication

---
+++ src/main/java/com/tax/TaxApplication.java
@@ -0,0 +1,12 @@
+package com.tax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.tax.TaxApplication.class, args);
+    }
+}
